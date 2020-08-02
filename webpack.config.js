115cc8db1e6661a20ed8f480a4d218c75210e87a
ins21 <leonnby@gmail.com>
2020-08-02 20:52:38 +0300

    add basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDevMode = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/index.js'],
+  devtool: isDevMode ? 'source-map' : '',
+  optimization: isDevMode ? {} : { minimizer: [ new OptimizeCssAssetWebpackPlugin(), new TerserWebpackPlugin() ]},
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: !isDevMode
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDevMode,
+              reloadAll: true
+            }
+          }, 
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|svf|gif)$/,
+        use: ['file-loader']
+      },
+      { 
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+    ]
+  },
+
+  devServer: {
+    port: 4200,
+    hot: isDevMode
+  }
+}
